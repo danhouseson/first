742fb09d9ebf6cafcb7bb5aee5fae44fcfad843f
DanHouse <danhouseson@gmail.com>
2017-09-12 19:11:54 +0200

    whatevermsg

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Initializing...')
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np
+
+hello = tf.constant('asdasd')
+sess = tf.Session()
+print(sess.run(hello))
